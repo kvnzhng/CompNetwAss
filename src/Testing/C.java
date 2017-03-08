2901f14dc9b4e5a5b39803f0cd68ef8dda4ecdbb
Kevin Zhang <kevinzhang230@hotmail.com>
2017-03-08 11:00:18 +0100

    Test

---
+++ src/Testing/C.java
@@ -0,0 +1,28 @@
+package Testing;
+/**
+ * Created by KevinZh on 08/03/2017.
+ */
+import java.io.*;
+import java.net.*;
+
+public class C {
+
+    public static String getHTML(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            result.append(line);
+        }
+        rd.close();
+        return result.toString();
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println(getHTML("http://www.google.com"));
+    }
+}
