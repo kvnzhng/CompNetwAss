fe9013d8eca6f353d86d8d08a684ff82b1634fae
Eleanor <Eleanor.vanlooy@telenet.be>
2017-03-13 14:26:35 +0100

    check args

--- src/Testing/TCPClient.java
+++ src/Testing/TCPClient.java
@@ -8,30 +8,46 @@ class TCPClient {
 
     public static void main(String[] args) throws Exception {
 
-        /*try {
-            TCPClient("www.google.com");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }*/
-        if (args.length == 2) {//zo moet het denk ik voor de cmd line
-            try {
-                TCPClient(args[0], args[1]);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        if (args.length == 3) {
-            try {
-                TCPClient(args[0], args[1], args[2]);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        String command;
+        if (isImplementedCommand(args[0])) {
+            command = args[0];
 
+            try {
+                InetAddress.getByName(args[1]);
+            } catch (UnknownHostException uhe) {
+                System.out.println("Unknown Host");
+            }
+            String url = args[1];
+
+
+            if (args.length == 2) {//zo moet het denk ik voor de cmd line
+                try {
+                    TCPClient(command, url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (args.length == 3) {
+                try {
+                    Integer.parseInt(args[2]);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Port is not a number");
+                }
+                String port = args[2];
+
+                try {
+                    TCPClient(command, url, port);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        else
+            System.out.println("501 not implemented");
     }
 
     public static void TCPClient(String command, String url) throws Exception {
-        TCPClient(command, url,null, Integer.toString(80));
+        TCPClient(command, url,null);
     }
 
     public static void TCPClient(String command, String url, String port) throws Exception {
@@ -41,7 +57,9 @@ class TCPClient {
     public static void TCPClient(String command, String url, String loc, String port) throws Exception {
 
         InetAddress addr = InetAddress.getByName(url);
-        Socket clientSocket = new Socket(addr, 80);
+        if (port == null)
+            port = "80";
+        Socket clientSocket = new Socket(addr, Integer.parseInt(port));
 
         PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
 
@@ -99,14 +117,45 @@ class TCPClient {
                 System.out.println(t);
         }
 
+
         pw.close();
         br.close();
         clientSocket.close();
     }
 
-    private void get(String url) {
-
+    private static boolean isImplementedCommand(String command) {
+        return (command.equals("GET") || command.equals("HEAD") ||command.equals("POST") ||command.equals("PUT"));
     }
+
+    /*private static void get(Socket clientSocket) {
+        boolean redirect = false;
+        BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        String t;
+        while((t = br.readLine()) != null) {
+            String domain=null;
+            String location=null;
+            if (t.contains("302"))
+            //TODO: andere exceptions kunnen opvangen.
+            {
+                //redirection
+                while ((t=br.readLine()) != null){
+                    if (t.contains("Location:")){
+                        String[] parts2 = t.split(" ");
+                        String redirectLoc = parts2[1];
+                        String[] parts3 = redirectLoc.split("/");
+                        redirect = true;
+                        domain = parts3[2];
+                        location = parts3[3];
+                        break;
+                    }
+                }
+            }
+            if (redirect)
+                TCPClient(command, domain,location, port);
+            else
+                System.out.println(t);
+        }
+    }*/
 
     private void head(String url) {
 
